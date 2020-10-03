9047ed30916049f508a1e50b9e32444f210822d5
Ivan <ivantbv@gmail.com>
2020-10-03 20:22:33 +0500

    fixed a bug between the get random button and the gradient tilt slider - the slider wasnt getting the updated body styles, instead it was getting the values of the color inputs only.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const body = document.getElementsByTagName('body');
 const insertBef = document.querySelector('#insert');
 const randomCol = document.querySelector('#random');
 const degreesSlider = document.querySelector('#degrees');
+let buttonRand;
 
 const hexToRgb = hex =>
   hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
@@ -20,44 +21,68 @@ const hexToRgb = hex =>
     document.body.insertBefore(copyBtn, insertBef)
 
 colOneInput.addEventListener('input', function() {
-
+    buttonRand = false;
     let toRgbOne = hexToRgb(colOneInput.value);
     let toRgbTwo = hexToRgb(colTwoInput.value);
    document.body.style.background = `linear-gradient(to left, ${colTwoInput.value}, ${colOneInput.value})`
+   //currentCol.textContent = document.body.style.background;
    currentCol.textContent = `background: linear-gradient(to left, rgb(${toRgbOne}), rgb(${toRgbTwo}))`;
 })
 
 colTwoInput.addEventListener('input', () => {
-   // document.body.insertBefore(copyBtn, insertBef)
-
+    buttonRand = false;
     let toRgbOne = hexToRgb(colOneInput.value);
     let toRgbTwo = hexToRgb(colTwoInput.value);
    document.body.style.background = `linear-gradient(to right, ${colOneInput.value}, ${colTwoInput.value})`
 
-currentCol.textContent = `background: linear-gradient(to right, rgb(${toRgbOne}), rgb(${toRgbTwo}))`;
+   //currentCol.textContent = document.body.style.background;
+
+   currentCol.textContent = `background: linear-gradient(to right, rgb(${toRgbOne}), rgb(${toRgbTwo}))`;
 })
 
 let randomColor = randomizeCol();
 let randomColor2 = randomizeCol();
+let thisColor;
+
+let randomC1;
+let randomC2;
 
 randomCol.addEventListener('click', () => {
-    document.body.insertBefore(copyBtn, insertBef)
-   let thisColor = document.body.style.background = `linear-gradient(to right, ${randomizeCol(colOneInput.value)}, ${randomizeCol(colTwoInput.value)})`
+    //document.body.insertBefore(copyBtn, insertBef)
+   randomC1 = randomizeCol(colOneInput.value);
+   randomC2 = randomizeCol(colTwoInput.value);
+   thisColor = document.body.style.background = `linear-gradient(to right, ${randomC1}, ${randomC2})`
 // let replaceLetters = thisColor.replace('linear-gradient(to right, ', '');
 // let popLastParanth = Array.from(replaceLetters).slice(0, -1);
-//currentCol.textContent = `${popLastParanth.join('')}`; //to display only both RGB values without linear-gradient property
-currentCol.textContent = `background: ${thisColor}`;
+//currentCol.textContent = `${popLastParanth.join('')}`; //to display only RGB values without linear-gradient property
+
+currentCol.textContent = document.body.style.background;
+//currentCol.textContent = `background: ${thisColor}`;
+buttonRand = true;
 })
+
 
 copyBtn.addEventListener('click', () => { 
     copy(currentCol.textContent)
 })
 
 degreesSlider.addEventListener('input', (e) => {
+    //numInputDegrees.value = e.target.value;
     let toRgbOne = hexToRgb(colOneInput.value);
     let toRgbTwo = hexToRgb(colTwoInput.value);
-    document.body.style.background = `linear-gradient(${e.target.value}deg, ${colTwoInput.value}, ${colOneInput.value})`
-    currentCol.textContent = `background: linear-gradient(${e.target.value}deg, rgb(${toRgbOne}), rgb(${toRgbTwo}))`;
+
+    if (buttonRand == true) {
+    document.body.style.background = `linear-gradient(${e.target.value}deg, ${randomC1}, ${randomC2})`
+    currentCol.textContent = document.body.style.background;
+    //`background: linear-gradient(${e.target.value}deg, rgb(${document.body.style.background}), rgb(${document.body.style.background}))`;
+    } else if (buttonRand == false) {
+        document.body.style.background = `linear-gradient(${e.target.value}deg, ${colOneInput.value}, ${colTwoInput.value})`
+        currentCol.textContent = document.body.style.background;
+        //`background: linear-gradient(${e.target.value}deg, rgb(${}), rgb(${document.body.style.background}))`;
+    } else {
+        document.body.style.background = `linear-gradient(${e.target.value}deg, ${colOneInput.value}, ${colTwoInput.value})`
+        currentCol.textContent = `background: linear-gradient(${e.target.value}deg, rgb(${toRgbOne}), rgb(${toRgbTwo}))`;
+    }
 })
 
 function randomizeCol() {
